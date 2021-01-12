1138865aa787b4affce9863c1dfbd0c2ef916209
unknown <k.pavlutska@gmail.com>
2021-01-12 21:32:34 +0200

    merge from remote develop

--- src/js/test.js
+++ src/js/test.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-const newProduct = {
-    name: "new product for testing purposes",
-    currentPrice: 199.99,
-    previousPrice: 250,
-    categories: "men",
-    imageUrls: [
-        "img/products/men/001.png",
-        "img/products/men/002.png",
-        "img/products/men/003.png",
-        "img/products/men/004.png"
-    ],
-    quantity: 100,
-    color: "red",
-    productUrl: "/men",
-    brand: "braaaand",
-    myCustomParam: "some string or json for custom param"
-};
-
-// axios
-//     .post("http://localhost:5000/products", newProduct)
-//     .then(newProduct => {
-//         console.log(newProduct)
-//         /*Do something with newProduct*/
-//     })
-//     .catch(err => {
-//         /*Do something with error, e.g. show error to user*/
-//     });
-
-// fetch('/products')
-//     .then(response => response.json())
-//     .then(json => console.log(json));
-=======
 fetch('/products')
     .then(response => response.json())
     .then(json => console.log(json));
->>>>>>> 7af2bf94a42541478768e70550616acce32402f5

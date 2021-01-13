10d332429b7a2d36cc49e896a5d5e3692d2ff54b
stas-lyu <staslyu1997@gmail.com>
2021-01-13 20:05:26 +0200

    add id json

--- src/js/modalWindow.js
+++ src/js/modalWindow.js
@@ -1,28 +1,24 @@
-let modal = document.getElementById("my_modal");
-let btn = [...document.querySelectorAll('.btn_by')];
-let span = document.getElementsByClassName("close_modal_window")[0];
-let close = document.querySelector('#cansel');
-
-console.log(btn);
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    for( let key in btn ){
-
-        btn[key].onclick = function(){
-            modal.style.display = "block";
-        }
-    }
-
-    span.onclick = function () {
-        modal.style.display = "none";
-    };
-
-    close.onclick = function () {
-        modal.style.display = "none";
-    };
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "block";
-        }
-    };
-});
+// let modal = document.getElementById("my_modal");
+// let btn = document.querySelectorAll('.btn_by');
+// let span = document.getElementsByClassName("close_modal_window")[0];
+// let close = document.querySelector('#cansel');
+//
+// for( let key in btn ){
+//     btn[key].onclick = function(e){
+//         modal.style.display = "block";
+//         console.log(e)
+//     }
+// }
+//
+//  span.onclick = function () {
+//     modal.style.display = "none";
+//  }
+//
+//  close.onclick = function () {
+//     modal.style.display = "none";
+//  }
+//  window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// };
--- src/js/newProducts.js
+++ src/js/newProducts.js
@@ -136,7 +136,32 @@ axios
             $(window).on('resize', () => changeArrow(), changeLink(products, `Все новинки`), changeLink(recommend, `Все рекомендации`), changeLink(sale, `Все товары`));
         });
     })
+    .then((data) => {
+        let modal = document.getElementById("my_modal");
+        let btn = document.querySelectorAll('.btn_by');
+        let span = document.getElementsByClassName("close_modal_window")[0];
+        let close = document.querySelector('#cansel');
 
+        for( let key in btn ){
+            btn[key].onclick = function(e){
+                modal.style.display = "block";
+                console.log(e)
+            }
+        }
+
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        close.onclick = function () {
+            modal.style.display = "none";
+        }
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        };
+    })
     .catch(err => {
         console.log(err)
         /*Do something with error, e.g. show error to user*/

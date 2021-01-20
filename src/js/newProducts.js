b0d6aabf80818449ee58dffa5c7c99e57acc352a
stas-lyu <staslyu1997@gmail.com>
2021-01-20 16:14:15 +0200

    dawdwa

--- src/js/newProducts.js
+++ src/js/newProducts.js
@@ -96,6 +96,24 @@ axios
         window.onclick = function (event) {
             if (event.target == modal) {
                 modal.style.display = "none";
+                let name = $('input[name=fio]').val();
+                let tel = $('input[name=tel]').val();
+                let send = true;
+                if (name == "") {
+                    send = false;
+                } else if (tel == "") {
+                    send = false
+                }
+                if (send) {
+                    let userInfo = {
+                        'user_name': name,
+                        'user_tel': tel,
+                    };
+                    $.post('send.php', userInfo, function (response) {
+                        let result = '<div style="color:#D80018;">' + response.text + '</div>';
+                        $("#form_result").hide().html(result).slideDown();
+                    }, 'json');
+                }
                 deleteCard();
             }
         };
@@ -284,26 +302,4 @@ export function item_modal_window(data) {
 
         }
     }
-    $("#order").click(() => {
-        let name = $('input[name=fio]').val();
-        let tel = $('input[name=tel]').val();
-        let send = true;
-        if (name == "") {
-            send = false;
-        } else if (tel == "") {
-            send = false
-        }
-
-        if (send) {
-            let userInfo = {
-                'user_name': name,
-                'user_tel': tel,
-            };
-
-            $.post('send.php', userInfo, function (response) {
-                let result = '<div style="color:#D80018;">' + response.text + '</div>';
-                $("#form_result").hide().html(result).slideDown();
-            }, 'json');
-        }
-    });
 }

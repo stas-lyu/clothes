8134f3fe7f9a8862d50c9faec4b077c0dca30736
stas-lyu <staslyu1997@gmail.com>
2021-01-20 13:24:33 +0200

    dawdwa

--- src/js/newProducts.js
+++ src/js/newProducts.js
@@ -284,5 +284,26 @@ export function item_modal_window(data) {
 
         }
     }
+    $("#order").click(() => {
+        let name = $('input[name=fio]').val();
+        let tel = $('input[name=tel]').val();
+        let send = true;
+        if (name == "") {
+            send = false;
+        } else if (tel == "") {
+            send = false
+        }
 
+        if (send) {
+            let userInfo = {
+                'user_name': name,
+                'user_tel': tel,
+            };
+
+            $.post('send.php', userInfo, function (response) {
+                let result = '<div style="color:#D80018;">' + response.text + '</div>';
+                $("#form_result").hide().html(result).slideDown();
+            }, 'json');
+        }
+    });
 }

f6afa94b34995bf2ca12346fb0b8fa11bbdd3663
stas-lyu <staslyu1997@gmail.com>
2021-01-27 20:16:18 +0200

    add cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -31,8 +31,8 @@ let shoppingCart = (function () {
 
     obj.addItemToCart = function (name, price, count) {
         for (let item in cart) {
-            if(cart[item].name === name) {
-                cart[item].count ++;
+            if (cart[item].name === name) {
+                cart[item].count++;
                 saveCart();
                 return
             }
@@ -43,12 +43,73 @@ let shoppingCart = (function () {
         saveCart()
     };
 
+    // Set count from item
+    obj.setCountForItem = function (name, count) {
+        for (let i in cart) {
+            if (cart[i].name === name) {
+                cart[i].count = count;
+                break;
+            }
+        }
+    };
+
     //Remove item from cart
     obj.removeItemFromCart = function (name) {
         for (let item in cart) {
-            if(cart[item].name === name) {
-                cart[item].count --;
+            if (cart[item].name === name) {
+                cart[item].count--;
+                if (cart[item].count === 0) {
+                    cart.splice(item, 1)
+                }
+                break;
             }
         }
-    }
-});
+        saveCart();
+    };
+
+    // Clear cart
+    obj.clearCart = function () {
+        cart = [];
+        saveCart();
+    };
+
+    //Count cart
+    obj.totalCount = function () {
+        let totalCount = 0;
+        for (let item in cart) {
+            totalCount += cart[item].count;
+        }
+        return totalCount
+    };
+
+    //Total cart
+
+    obj.totalCart = function () {
+        let totalCart = 0;
+        for (let item in cart) {
+            totalCart += cart[item].price * cart[item].count
+        }
+        return Number(totalCart.toFixed(2))
+    };
+
+    //List cart
+
+    // List cart
+    obj.listCart = function () {
+        let cartCopy = [];
+        for (let i in cart) {
+            let item = cart[i];
+            let itemCopy = {};
+            for (let p in item) {
+                itemCopy[p] = item[p];
+
+            }
+            itemCopy.total = Number(item.price * item.count).toFixed(2);
+            cartCopy.push(itemCopy)
+        }
+        return cartCopy;
+    };
+
+    return obj;
+
+})();
